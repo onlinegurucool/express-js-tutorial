3dec155f27bd763e7eb1cca33d6eae36333a3762
Pratap Rockerss <pratap@techcetra.com>
2019-10-16 07:20:02 +0530

    multipleer handler added

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,31 @@ router.post("/",(req,res) => {
 })
 
 
-router.post("/get-data-from-client/:category/:product_id",(req,res) => {
+router.post("/get-data-from-client/:category/:product_id",
+    (req,res,next) => {
+        let index = 2
+        if(!req.body.user_id) {
+            res.send({
+                msg: "Validation Error",
+                error: "User ID is required"
+            })
+        } else {
+            req.nextOptions = {
+                indexInNextFunction: index,
+                newKey: 2
+            }
+            next();
+        }
+    },
+    (req,res) => {
     console.log(`query ${JSON.stringify(req.query)}`);
     console.log(`params ${JSON.stringify(req.params)}`);
     console.log(`body ${JSON.stringify(req.body)}`);
+    res.send({
+        msg: "success",
+        data: req.body,
+        nextOptions: req.nextOptions
+    })
 })
 
 
