0f975c6aab858ff9f8fd924899dc864532497081
Pratap Rockerss <pratap@techcetra.com>
2020-05-02 18:16:25 +0530

    Basic Validation added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const userModel = require('../models/user')
+const userModel = require('../models/user');
 
-router.post("/create", async (req, res) => {
+let form = require('express-form');
+let field = form.field;
+
+router.post("/create",
+    form(
+        field("first_name").trim().required("","First Name is mandatory"),
+        field("last_name").trim().required(),
+        field("mobile").trim().required().isNumeric(),
+        field("email").trim().required().isEmail(),
+    ), async (req, res) => {
+        if(!req.form.isValid) {
+            return res.send({
+                msg: "Validation Error",
+                error: req.form.getErrors()
+            })
+        }
     let data = {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
