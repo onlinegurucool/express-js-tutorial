32a850d02a9724bb2de1957b6253fcb9fe8afda2
Pratap Rockerss <pratap@techcetra.com>
2020-05-03 13:14:18 +0530

    basic files added for

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const con = require("../config/db");
 
 module.exports = {
-    create: data => {
-        return new Promise(function(resolve, reject) {
+    create: (data) => {
+        return new Promise(function (resolve, reject) {
             con.query(`INSERT INTO user SET ? `, [data], (err, result) => {
                 if (err) {
                     reject(err);
@@ -12,7 +12,42 @@ module.exports = {
             });
         });
     },
-    read: () => {},
-    update: () => {},
-    delete: () => {}
+    read: (user_id) => {
+        return new Promise(function (resolve, reject) {
+            let query = `SELECT * FROM user u where  1 = 1`;
+            if (user_id) {
+                query += ` AND u.id ="${user_id}" `;
+            }
+            con.query(query, (error, rows) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    },
+    update: (data, user_id) => {
+        return new Promise((resolve, reject) => {
+            con.query(
+                `
+                UPDATE 
+                    user u
+                SET 
+                    ?
+                WHERE 
+                    u.id = ?
+            `,
+                [data, user_id],
+                (error, status) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(status);
+                    }
+                }
+            );
+        });
+    },
+    delete: () => {},
 };
--- routes/users.js
+++ routes/users.js
@@ -3,57 +3,22 @@ const router = express.Router();
 
 const userModel = require("../models/user");
 
-let form = require("express-form");
-let field = form.field;
-
-router.post(
-    "/create",
-    form(
-        field("first_name").trim().required("", "First Name is mandatory"),
-        field("last_name").trim().required(),
-        field("mobile")
-            .trim()
-            .required("", "Mobile is mandatory")
-            .isNumeric()
-            .custom((value, source, cb) => {
-                let num = value.toString();
-                if (num.length < 10 || num.length > 11) {
-                    return cb(
-                        new Error("Mobile number is to short or to Long")
-                    );
-                }
-                if (num.length == 11 && num[0] != 0) {
-                    return cb(new Error("Mobile Number is invalid"));
-                }
-                return cb(null)
-            }),
-        field("email").trim().required().isEmail()
-    ),
-    async (req, res) => {
-        if (!req.form.isValid) {
-            return res.send({
-                msg: "Validation Error",
-                error: req.form.getErrors(),
-            });
-        }
+router.post("/edit/:user_id", async (req, res) => {
+    try {
         let data = {
             first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            mobile: req.body.mobile,
-            email: req.body.email,
         };
-        let resp = {};
-        let err = {};
-        try {
-            resp = await userModel.create(data);
-        } catch (error) {
-            err = error;
-        }
+        let resp = await userModel.update(data, req.params.user_id);
         res.send({
-            resp: resp,
-            error: err,
+            msg: "User Updated successfully",
+            resp,
+        });
+    } catch (error) {
+        res.send({
+            msg: error.message,
+            error,
         });
     }
-);
+});
 
 module.exports = router;
